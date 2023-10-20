055f80ceb2fa34d5575817fc11d2dcc93597b58a
AJ <105425724+AbhishekJamkar@users.noreply.github.com>
2023-10-20 22:37:14 +0530

    Add files via upload

---
+++ Cab-Booking/lib/globalvariables.dart
@@ -0,0 +1,9 @@
+import 'package:cab_rider/datamodels/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+String mapKey = MAPKEY;
+String tempKey = MAPKEY1;
+
+User? currentFirebaseUser;
+Userclass? currUserInfo;
+
